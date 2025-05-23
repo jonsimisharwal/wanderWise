ff8f5531d0f84188bb1e783ce7c67104608fd9d9
jonsi <jonsimisharwal@gmail.com>
2025-05-24 00:27:15 +0530

    Initial commit

---
+++ Frontend/Wonderwise/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className="text-4xl text-teal-600 font-bold">Traveler Project by Jony 💼🌍</h1>
+
+      </>
+  )
+}
+
+export default App
